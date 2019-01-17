dd594bca6be99e36e671a20579cbdf1f546434fd
Antony Dominic <antony.david@libertymutual.com>
2019-01-17 09:15:26 -0500

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,8 +10,8 @@ class Quiz extends Component {
     render() {
         return (
             <div><div className="QuizQuestion">{quizData.quiz_questions[0]
-            .instruct_text}</div></div>
-                )
+            .instruction_text}</div></div>
+        )
     }
 }
 
