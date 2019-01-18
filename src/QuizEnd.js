da13643804bd09e11b15b7fc33bc8608d3e262a3
Antony Dominic <antony.david@libertymutual.com>
2019-01-18 16:28:36 -0500

    committ

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion.js'
-import QuizEnd from './QuizEnd.js'
 
-let quizData = require('./quiz_data.json')
-
-class Quiz extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { quiz_position: 1 }
-  }
+class QuizEnd extends Component {
   render() {
-      const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
-      return (
-          <div>
-              {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
-          </div>
-      )
+    return (
+      <div>
+        <p>Thanks for playing!</p>
+        <a> href='#'>Reset Quiz</a>
+      </div>          
+    )    
   }
 }
 
-export default Quiz
+export default QuizEnd
